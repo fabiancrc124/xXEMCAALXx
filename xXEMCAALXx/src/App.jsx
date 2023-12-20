7adfb3b37e6f2f4b9f349131b471fe49ce0b66e8
fabiancrc124 <fabiancrc124@gmail.com>
2023-12-20 00:29:02 -0500

    segundo commit

--- xXEMCAALXx/src/App.jsx
+++ xXEMCAALXx/src/App.jsx
@@ -1,10 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
 import Login from './Components/login';
 import { CotizadorProvider } from "./context/CotizadorProvider"
-
-
-
 
 function App() {
 
